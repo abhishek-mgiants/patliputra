b34d8219f737a4cc81d1fd684be1f629279eb1b3
Abhishek Shukla <abhishek@mgiants.com>
2023-08-08 12:29:35 +0530

    react-router testing

--- src/App.jsx
+++ src/App.jsx
@@ -17,52 +17,15 @@ import Contact from './components/HomeContact/Contact'
 export default function App() {
   return (
    <>
-    {/* <Navbar/> */}
-     <Home/>
-     <About/>
-
-     <AnimatedObjects/>
-
-     <Clients/>
-     <AboutUs/>
-     <Services2/>
-     <Whyus/>
-
-     <div>
-      <div className="map-container"
-      style={{ position:'relative'}}
-      >
-      <img src="/map.png" style={{width:'100%', height:'100%'}} alt="" />
-      <div className="map-image-container"
-      style={{
-        position:'absolute',
-        width:'300px',
-        height:'350px',
-        overflow:'hidden',
-        border:'2px solid rgb(122, 122, 122)',
-        boxShadow:'0px 5px 10px black',
-        top:'20vh',
-        right:'10vw',
-        padding:'20px',
-        backgroundColor:'#e4e3ec'
-
-      }}
-      >
-        <h1>Locations</h1>
-        <ul>
-          <li>Gurgaon</li>
-          <li>Noida</li>
-          <li>Delhi</li>
-          <li>Mumbai</li>  
-        </ul>
-      </div>
-      </div>
-      
-     </div>
-     <Contact/>
-     <Services/>
-     <ContactUs/> 
-     <Footer/>
+    <BrowserRouter  basename={import.meta.env.DEV ? '/' : '/patliputra/'}>
+             
+        <Routes>
+        <Route exact path='/home' element={ <div> <h1>Hello</h1> </div> } />
+        <Route path='/about' element={  <div>About</div> }/>
+        </Routes>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+    </BrowserRouter>
    </>
   )
 }
